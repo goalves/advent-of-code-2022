2102a9f6be63a4cd472fbffa92856ecb9bfea440
Gabriel Alves <g04lves@gmail.com>
2022-12-01 09:49:11 -0800

    day 1 challenge

---
+++ day_1/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut results: Vec<u64> = include_str!("../input")
+        .split("\n\n")
+        .into_iter()
+        .map(|calories_str| {
+            calories_str
+                .split("\n")
+                .map(|x| x.parse::<u64>().unwrap_or(0))
+                .sum::<u64>()
+        })
+        .collect();
+
+    results.sort_unstable_by(|a, b| b.cmp(a));
+
+    println!("{}", results.iter().take(3).sum::<u64>());
+}
