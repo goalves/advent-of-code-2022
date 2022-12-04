2e6e6fff01e08180b98788f48cac1daf0d7e22d6
Gabriel Alves <g04lves@gmail.com>
2022-12-04 12:31:45 -0800

    refactor day4 to make it cleaner

--- src/bin/day-4.rs
+++ src/bin/day-4.rs
@@ -1,26 +1,26 @@
-use itertools::Itertools;
-
 #[derive(Debug)]
 struct ElfPair {
     sections: Vec<ElfSection>,
 }
 
 impl ElfPair {
-    pub fn from_entry(entry: &str) -> Self {
-        let sections = entry
-            .split(",")
-            .map(|x| ElfSection::from_entry(x))
-            .collect::<Vec<ElfSection>>();
-
-        Self { sections }
-    }
-
     pub fn has_self_containment(&self) -> bool {
         self.sections[0].contains(&self.sections[1]) || self.sections[1].contains(&self.sections[0])
     }
 
     pub fn has_overlaps(&self) -> bool {
         self.sections[0].overlaps(&self.sections[1])
+    }
+}
+
+impl From<&str> for ElfPair {
+    fn from(entry: &str) -> Self {
+        let sections = entry
+            .split(',')
+            .map(ElfSection::from)
+            .collect::<Vec<ElfSection>>();
+
+        Self { sections }
     }
 }
 
@@ -31,47 +31,44 @@ struct ElfSection {
 }
 
 impl ElfSection {
-    pub fn from_entry(entry: &str) -> Self {
-        let data: Vec<&str> = entry.split("-").collect();
+    pub fn contains(&self, other: &ElfSection) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    pub fn overlaps(&self, other: &ElfSection) -> bool {
+        self.end >= other.start && self.start <= other.end
+    }
+}
+
+impl From<&str> for ElfSection {
+    fn from(entry: &str) -> Self {
+        let data: Vec<&str> = entry.split('-').collect();
 
         Self {
             start: data[0].parse::<u32>().unwrap(),
             end: data[1].parse::<u32>().unwrap(),
         }
     }
-
-    pub fn contains(&self, other: &ElfSection) -> bool {
-        self.start <= other.start && self.end >= other.end
-    }
-
-    pub fn overlaps(&self, other: &ElfSection) -> bool {
-        println!("{:?}, {:?}: {}", self, other, self.end >= other.start);
-
-        self.end >= other.start && self.start <= other.end
-    }
 }
 
 fn main() {
-    let input_data = include_str!("../../day4_input");
-
-    let elf_pairs = input_data
-        .lines()
-        .map(|line| ElfPair::from_entry(line))
-        .collect::<Vec<ElfPair>>();
+    let elf_pairs = include_str!("../../day4_input").lines().map(ElfPair::from);
 
     let first_phase = elf_pairs
-        .iter()
-        .map(|pair| if pair.has_self_containment() { 1 } else { 0 })
+        .clone()
+        .map(|pair| u32::from(pair.has_self_containment()))
         .sum::<u32>();
 
-    println!("{}", first_phase);
+    println!(
+        "Number of elf pairs with full containments: {}",
+        first_phase
+    );
 
     let second_phase = elf_pairs
-        .iter()
-        .map(|pair| if pair.has_overlaps() { 1 } else { 0 })
+        .map(|pair| u32::from(pair.has_overlaps()))
         .sum::<u32>();
 
-    println!("{}", second_phase);
+    println!("Number of elf pairs with overlaps: {}", second_phase);
 }
 
 #[cfg(test)]
@@ -80,11 +77,9 @@ mod tests {
 
     #[test]
     fn first_half() {
-        let input_data = include_str!("../../test_inputs/day4_test");
-
-        let elf_pairs = input_data
+        let elf_pairs = include_str!("../../test_inputs/day4_test")
             .lines()
-            .map(|line| ElfPair::from_entry(line))
+            .map(|line| ElfPair::from(line))
             .collect::<Vec<ElfPair>>();
 
         assert_eq!(6, elf_pairs.len());
@@ -95,6 +90,16 @@ mod tests {
             .sum::<u32>();
 
         assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn second_half() {
+        let elf_pairs = include_str!("../../test_inputs/day4_test")
+            .lines()
+            .map(|line| ElfPair::from(line))
+            .collect::<Vec<ElfPair>>();
+
+        assert_eq!(6, elf_pairs.len());
 
         let result = elf_pairs
             .iter()
@@ -103,7 +108,4 @@ mod tests {
 
         assert_eq!(result, 4);
     }
-
-    #[test]
-    fn second_half() {}
 }
