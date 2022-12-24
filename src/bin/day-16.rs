a35eba1ee81a1e6af441a93cff320f0f58507943
Gabriel Alves <g04lves@gmail.com>
2022-12-24 10:06:48 -0800

    day 16 phase2 idea?

--- src/bin/day-16.rs
+++ src/bin/day-16.rs
@@ -9,39 +9,50 @@ fn find_best(
     nodes: &HashMap<String, Valve>,
     minutes: i64,
     cache: &mut HashMap<VisitedPathWithCost, i64>,
-) -> i64 {
+) -> (i64, Vec<String>) {
     if minutes <= 0 {
-        return 0;
+        return (0, path.clone());
     }
 
     if let Some(&cached_cost) = cache.get(&(node.clone(), path.clone(), minutes)) {
-        return cached_cost;
+        return (cached_cost, path.clone());
     }
 
     let mut best = i64::MIN;
+    let mut best_path = path.clone();
     let current_flow_rate = nodes.get(&node).unwrap().flow_rate;
 
     // if current node has flow, always stop to open it and we didn't open it yet we need to open it (2 mins)
     if current_flow_rate > 0 && !path.contains(&node) {
         for child in &nodes.get(&node).unwrap().tunnels {
             path.push(node.clone());
-            let child_pressure = find_best(child.to_string(), path, nodes, minutes - 2, cache);
+            let (child_pressure, child_best_path) =
+                find_best(child.to_string(), path, nodes, minutes - 2, cache);
 
-            best = best.max(child_pressure + current_flow_rate * (minutes - 1));
+            let child_best = child_pressure + current_flow_rate * (minutes - 1);
+            if best < child_best {
+                best_path = child_best_path;
+                best = child_best;
+            }
+
             path.pop();
         }
     }
 
     // for any other node, go to it and then calculate the best value from all its branches
     for child in &nodes.get(&node).unwrap().tunnels {
-        let sub_result = find_best(child.to_string(), path, nodes, minutes - 1, cache);
-        best = best.max(sub_result);
+        let (child_pressure, child_best_path) =
+            find_best(child.to_string(), path, nodes, minutes - 1, cache);
+        if best < child_pressure {
+            best = child_pressure;
+            best_path = child_best_path;
+        }
     }
 
     // uses cache to avoid revisitting paths that were already visited
     cache.insert((node, path.clone(), minutes), best);
 
-    best
+    (best, best_path)
 }
 
 #[derive(Debug)]
@@ -83,13 +94,23 @@ fn parse_valves(input: &str) -> Result<HashMap<String, Valve>, &'static str> {
 }
 
 fn main() {
-    let input = include_str!("../../day16_input");
-    // let input = include_str!("../../test_inputs/day16_test");
+    // let input = include_str!("../../day16_input");
+    let input = include_str!("../../test_inputs/day16_test");
     let valves = parse_valves(input).unwrap();
 
-    let mut path = vec![];
-    let remaining: i64 = 30;
-    let mut cache = HashMap::new();
-    let pressure = find_best("AA".to_string(), &mut path, &valves, remaining, &mut cache);
+    // First Phase
+    let pressure = find_best(
+        "AA".to_string(),
+        &mut vec![],
+        &valves,
+        30,
+        &mut HashMap::new(),
+    );
     println!("Pressure after 30 minutes: {:?}", pressure);
+
+    // Second Phase Idea:
+    // Instead of finding the BEST one from the find_best solution I can
+    // Get all of the paths, then find the disjoint ones (I won't ever open the same valve twice)
+    // If I sum the pressure released sets I can then find the pressure for the second actor
+    // (I can even do it for N elephants possibly?)
 }
